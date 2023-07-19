1963d7f0fc63c5ae820a9d503cc20d87c10d067c
Tarte <rtarte@avaya.com>
2023-07-20 03:15:53 +0530

    made changes

--- app.js
+++ app.js
@@ -14,61 +14,6 @@ let todos = [
   { id: 2, title: 'Task 2', completed: true },
 ];
 
-app.post('/mywebhook', (req, res) => {
-    console.log("reached POST endpoint")
-    console.log(req.body)
-    const reqJsonObj = req.body;
-    // newTodo.id = todos.length + 1;
-    // todos.push(newTodo);
-
-    let resJsonObj = {
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "themeColor": "0076D7",
-        "summary": "GCP Monitoring alert",
-        "sections": [{
-            "activityTitle": "GCP Monitoring alert",
-            "activitySubtitle": "On Project Tango",
-            "activityImage": "https://teamsnodesample.azurewebsites.net/static/img/image5.png",
-            "facts": [{
-                "name": "Policy Name",
-                "value": ""
-            }, {
-                "name": "Metric Name",
-                "value": ""
-            }, {
-                "name": "Condition name",
-                "value": ""
-            }],
-            "markdown": true
-        }]
-    }
-
-    console.log("resJsonObj: ", resJsonObj)
-
-    resJsonObj.sections[0].activitySubtitle = "On Project " + reqJsonObj.incident.scoping_project_id;
-    resJsonObj.sections[0].facts[0].value = reqJsonObj.incident.policy_name;
-    resJsonObj.sections[0].facts[1].value = reqJsonObj.incident.metric.displayName;
-    resJsonObj.sections[0].facts[2].value = reqJsonObj.incident.condition_name;
-
-    const webhookURL = "https://avaya365.webhook.office.com/webhookb2/036829e4-82f8-4a03-ac7b-737dc2cd44df@04a2636c-326d-48ff-93f8-709875bd3aa9/IncomingWebhook/84b56a0f89b844b5bdc4cef5b48d8e62/6165e0c6-6b67-47d1-847b-d54a2329b5c1";
-
-    axios.post(webhookURL, resJsonObj)
-    .then(response => {
-      // Handle the response from the POST request
-      console.log(response.data);
-      res.send('POST request successful');
-    })
-    .catch(error => {
-      // Handle the error if the POST request fails
-      console.error(error);
-      res.status(500).send('POST request failed');
-    });
-
-
-    // res.status(201).json(resJsonObj);
-  });
-
 // Get all todos
 app.get('/todos', (req, res) => {
   res.json(todos);
